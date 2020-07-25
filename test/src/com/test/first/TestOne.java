c6539ec94796b26a214c93a00bf29cfad492b15b
Sudhakar Vasiraju <sudhakar.vasiraju@bazaarvoice.com>
2020-07-25 02:54:10 -0500

    first file

---
+++ test/src/com/test/first/TestOne.java
@@ -0,0 +1,25 @@
+package com.test.first;
+
+public class TestOne {
+    public static void main(String[] str) {
+        new TestOne();
+    }
+
+    public TestOne(){
+        callM();
+    }
+
+    public void callM(){
+        sop("print!");
+        sopl("");
+        sopl("printing!!!!");
+    }
+
+    private void sop(Object o){
+        System.out.print(o);
+    }
+
+    private void sopl(Object o) {
+        System.out.println(o);
+    }
+}
